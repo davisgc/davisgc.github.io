292a2536b7f9a22969148b5d3de3eb3bf01ae35e
davisgc <davisgc@uindy.edu>
2023-10-10 21:56:45 -0400

    finished assignment
    should be good as is. might add the "pop cat" thing later

--- js/fetch.js
+++ js/fetch.js
@@ -1,14 +1,8 @@
-function ItemSeven(){
+function ItemFive(){
   fetch("http://35.188.215.226:8000/my_program", {
     method: "POST",
-    body: JSON.stringify({
-      name: "frank",
-      program: "all",
-      completed:false
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
+    name: "frank",
+    program: "all"
   })
     .then((response) => response.json())
     .then((json) => console.log(json));
--- js/onClick.js
+++ js/onClick.js
@@ -6,10 +6,11 @@ function postScript(){
     title[0].innerHTML = 'I don\'t even know.';
     title[0].innerHTML += '<div class="menuSubItem">\n';
     workspace[0].innerText = "Seriously, I've been trying for hours. What am I even doing?";
-    ItemSeven();
+    ItemFive();
 }
 
 function progLang(){
+    console.log("changed tabs");
     let title = document.getElementsByClassName("leftMenuItem");
     let workspace = document.getElementsByClassName("menuSubItem");
     title[0].innerHTML = 'Programming Languages';
@@ -18,6 +19,7 @@ function progLang(){
 }
 
 function WorkXP(){
+    console.log("changed tabs");
     let title = document.getElementsByClassName("leftMenuItem");
     let workspace = document.getElementsByClassName("menuSubItem");
     title[0].innerHTML = 'Work Experience';
@@ -26,9 +28,71 @@ function WorkXP(){
 }
 
 function Interests(){
+    console.log("changed tabs");
     let title = document.getElementsByClassName("leftMenuItem");
     let workspace = document.getElementsByClassName("menuSubItem");
     title[0].innerHTML = 'Interests';
     title[0].innerHTML += '<div class="menuSubItem">\n';
     workspace[0].innerText = "I've had an interest in algebraic math since middle school, and originally planned on majoring in it before suddenly gaining an interest in programmming in high school.\n\nI enjoy automating tasks on my computer, such as creating an application that would roll stats for my DnD character. This also extends to my interest in video games, two of my favorites being Satisfactory and Factorio, two games about automating tasks.";
+}
+
+function Project(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let workspace = document.getElementsByClassName("menuSubItem");
+    title[0].innerHTML = 'Project';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    workspace[0].innerHTML = "<p ondblclick='dblClickEx()'>Double Click Me</p>";
+    workspace[0].innerHTML += "<p oncontextmenu='contextEx()'>Right Click Me</p>";
+    workspace[0].innerHTML += "<p id='hoverEx' onmouseover=mouseOverEx() onmouseout='mouseAwayEx()'>Hover Over Me</p>";
+    workspace[0].innerHTML += "<p id='keyPressEx' onclick='keyDownEx()'>Shift-Click Me</p>";
+    workspace[0].innerHTML += "<p onclick='reloadPage()'>Click This To Reload</p>";
+}
+
+function fixFace(){
+    console.log("Fixed face");
+    let workspace = document.getElementsByClassName("mugshot");
+    workspace[0].innerHTML = '<img src="img/mugshot.png" alt="self-portrait">';
+}
+
+function dblClickEx(){
+    console.log("made face evil");
+    let workspace = document.getElementsByClassName("mugshot");
+    workspace[0].innerHTML = '<img src="img/mugstache.png" alt="self-portrait" ondblclick="fixFace()">';
+    alert("But nothing happens.");
+}
+
+function contextEx(){
+    console.log("right click changes the content font size");
+    let fonts = ["x-small", "small", "medium", "large", "x-large"];
+    let workspace = document.getElementsByClassName("content");
+    let r = Math.floor(Math.random() * fonts.length);
+    workspace[0].style.fontSize = fonts[r];
+    //console.log(r);
+}
+
+function mouseOverEx(){
+    console.log("mousing over changes the text");
+    let workspace = document.getElementById("hoverEx");
+    workspace.innerText = "The text changes while you hover over it!";
+}
+
+function mouseAwayEx(){
+    console.log("mousing away changes the text as well");
+    let workspace = document.getElementById("hoverEx");
+    workspace.innerText = "And changes back when you move away!";
+}
+
+function keyDownEx(){
+    console.log("changes the text depending on if shift was held")
+    let workspace = document.getElementById("keyPressEx");
+    if (event.shiftKey) {
+        workspace.innerText = "You clicked this while pressing Shift.";
+    } else {
+        workspace.innerText = "I said use Shift";
+    }
+}
+
+function reloadPage(){
+    location.reload();
 }
