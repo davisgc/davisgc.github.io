d854801519e5f00f8ed2190e4cf8147e50915897
davisgc <davisgc@uindy.edu>
2024-04-16 01:29:09 -0400

    draft

--- js/onClick.js
+++ js/onClick.js
@@ -1,122 +1,112 @@
-function postScript(){
-    // TODO
-    // Actually, I have no idea what I need to do here.
-    let title = document.getElementsByClassName("leftMenuItem");
-    let workspace = document.getElementsByClassName("menuSubItem");
-    title[0].innerHTML = 'I don\'t even know.';
-    title[0].innerHTML += '<div class="menuSubItem">\n';
-    workspace[0].innerText = "Seriously, I've been trying for hours. What am I even doing?";
-    ItemFive();
-}
-
-function progLang(){
+function evangelicalismTab(){
     console.log("changed tabs");
     let title = document.getElementsByClassName("leftMenuItem");
-    let workspace = document.getElementsByClassName("menuSubItem");
-    title[0].innerHTML = 'Programming Languages';
-    title[0].innerHTML += '<div class="menuSubItem">\n';
-    workspace[0].innerText = "I have several years of experience in C, C++, and Python.";
-}
-
-function WorkXP(){
-    console.log("changed tabs");
-    let title = document.getElementsByClassName("leftMenuItem");
-    let workspace = document.getElementsByClassName("menuSubItem");
-    title[0].innerHTML = 'Work Experience';
-    title[0].innerHTML += '<div class="menuSubItem">\n';
-    workspace[0].innerText = "I worked a couple of menial jobs throughout high school, leaving on good terms. I have been tutoring mathematics for UIndy since my Freshman year of college.";
-}
-
-function Interests(){
-    console.log("changed tabs");
-    let title = document.getElementsByClassName("leftMenuItem");
-    let workspace = document.getElementsByClassName("menuSubItem");
-    title[0].innerHTML = 'Interests';
-    title[0].innerHTML += '<div class="menuSubItem">\n';
-    workspace[0].innerText = "I've had an interest in algebraic math since middle school, and originally planned on majoring in it before suddenly gaining an interest in programmming in high school.\n\nI enjoy automating tasks on my computer, such as creating an application that would roll stats for my DnD character. This also extends to my interest in video games, two of my favorites being Satisfactory and Factorio, two games about automating tasks.";
-}
-
-function Project(){
-    console.log("changed tabs");
-    let title = document.getElementsByClassName("leftMenuItem");
-    let workspace = document.getElementsByClassName("menuSubItem");
-    title[0].innerHTML = 'Project';
-    title[0].innerHTML += '<div class="menuSubItem">\n';
-    workspace[0].innerHTML = "<p ondblclick='dblClickEx()'>Double Click Me</p>";
-    workspace[0].innerHTML += "<p oncontextmenu='contextEx()'>Right Click Me</p>";
-    workspace[0].innerHTML += "<p id='hoverEx' onmouseover=mouseOverEx() onmouseout='mouseAwayEx()'>Hover Over Me</p>";
-    workspace[0].innerHTML += "<p id='keyPressEx' onclick='keyDownEx()'>Shift-Click Me</p>";
-    workspace[0].innerHTML += "<p onclick='reloadPage()'>Click This To Reload</p>";
-}
-
-function PopCat(){
-    console.log("changed tabs");
-    let title = document.getElementsByClassName("leftMenuItem");
-    let workspace = document.getElementsByClassName("menuSubItem");
-    title[0].innerHTML = 'Feed the Cat';
-    title[0].innerHTML += '<div class="menuSubItem">\n';
-    workspace[0].innerHTML = "<img id='fish' src='./img/fish.jpg' width=128px onmousedown='mouseDownFish()'>";
-    workspace[0].innerHTML += "<br><p id='catface'><img src='./img/cat.jpg'></p>";
-}
-
-function fixFace(){
-    console.log("Fixed face");
-    let workspace = document.getElementsByClassName("mugshot");
-    workspace[0].innerHTML = '<img src="img/mugshot.png" alt="self-portrait">';
-}
-
-function dblClickEx(){
-    console.log("made face evil");
-    let workspace = document.getElementsByClassName("mugshot");
-    workspace[0].innerHTML = '<img src="img/mugstache.png" alt="self-portrait" ondblclick="fixFace()">';
-    alert("But nothing happens.");
-}
-
-function contextEx(){
-    console.log("right click changes the content font size");
-    let fonts = ["x-small", "small", "medium", "large", "x-large"];
+    let subtitle = document.getElementsByClassName("menuSubItem");
     let workspace = document.getElementsByClassName("content");
-    let r = Math.floor(Math.random() * fonts.length);
-    workspace[0].style.fontSize = fonts[r];
-    //console.log(r);
+    title[0].innerHTML = 'Evangelicalism:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "What makes it unique?";
+    workspace[0].innerHTML = "There are a few things that make Evangelicalism unique. For example <br> ";
+    workspace[0].innerHTML += "(1) Evangelicalism believe the Bible is ineffible. <br> ";
+    workspace[0].innerHTML += "<img src='./img/bible.jpg' height=120px><br><br>";
+    workspace[0].innerHTML += "(2) They have a much higher focus on Jesus than most other, if not all other, denomination.<br> ";
+    workspace[0].innerHTML += "<img src='./img/jesus.jpg' height=120px><br><br>";
+    workspace[0].innerHTML += "(3) They believe in being \"reborn\" through Baptism, hence \"Born-Again\" Christians.<br>";
+    workspace[0].innerHTML += "<img src='./img/cross2.jpg' height=120px><br>";
 }
 
-function mouseOverEx(){
-    console.log("mousing over changes the text");
-    let workspace = document.getElementById("hoverEx");
-    workspace.innerText = "The text changes while you hover over it!";
+function evangelicalismQuestion(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'Evangelicalism:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "Is It Popular?";
+    workspace[0].innerHTML = "Evangelicalism \"more than doctrinal distinctives and religious movements\" <br><br> ";
+    workspace[0].innerHTML += "By the definition providede by Hartford Institude for Religion Research, one-fourth of Americans are defined as Evangelical.<br><br> ";
+    workspace[0].innerHTML += "These include, but are not limited to, evangelical Protestants, mainline Protestants, black Protestants, and Roman Catholic.<br><br>";
+    workspace[0].innerHTML += "<br>(Swatos, 1998)<br>";
 }
 
-function mouseAwayEx(){
-    console.log("mousing away changes the text as well");
-    let workspace = document.getElementById("hoverEx");
-    workspace.innerText = "And changes back when you move away!";
+function evangelicalismExclaim(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'Evangelicalism:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "Why?";
+    workspace[0].innerHTML = "More of my own interpretation, but: <br><br> ";
+    workspace[0].innerHTML += "Evangelicalism is an escape from the complexity of the church.<br><br> ";
+    workspace[0].innerHTML += "When Luther would spread his booklets, they were described as \"short and direct\", and Evangelicalism seems to be an extension of this.<br><br>";
+    workspace[0].innerHTML += "Returning to a more \"casual\" relationship with Jesus is far more efficient.<br><br>";
+    workspace[0].innerHTML += "<br>(Cooper, 2022)<br>";
 }
 
-function keyDownEx(){
-    console.log("changes the text depending on if shift was held")
-    let workspace = document.getElementById("keyPressEx");
-    if (event.shiftKey) {
-        workspace.innerText = "You clicked this while pressing Shift.";
-    } else {
-        workspace.innerText = "I said use Shift";
-    }
+function FieldNotes(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'Field Notes:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "They Were Scanned";
+    workspace[0].innerHTML = "<img src='./img/fieldnotes.jpg' height=900px><br>";
 }
 
-function mouseDownFish(){
-    console.log("grabbed fish");
-    let workspace = document.getElementById("catface");
-    workspace.innerHTML = "<br><p id='catface'><img src=./img/pop.jpg onmouseup='mouseUpFish()'></p>";
+function WorksCited(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'Works Cited:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "This Is Required";
+    workspace[0].innerHTML = "Cooper, T. W. (2022). <i>The Digital Evangelicals: Contesting Authority and authenticity after the New Media Turn.</i> Indiana University Press. <br><br>";
+    workspace[0].innerHTML += "Swatos, W. H. (Ed.). (1998). <i>EVANGELICALISM</i>. Content pages of the Encyclopedia of Religion and Social Science. http://hirr.hartsem.edu/ency/evan.htm <br>";
 }
 
-function mouseUpFish(){
-    console.log("let go of fish");
-    let workspace = document.getElementById("catface");
-    workspace.innerHTML = "<br><p id='catface'><img src=./img/averageCat.jpg></p>";
-    let del = document.getElementById("fish");
-    del.src = "";
+function TheEvent(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'The Event:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "What Did I Attend?";
+    workspace[0].innerHTML = "I attended the livestreamed event from the group called \"Life.Church\". The event was livestreamed on their YouTube channel by the same name. It was titled <a href=\"https://www.youtube.com/watch?v=aMprbls910M\">Trusting God When You Don't Understand</a>";
+    workspace[0].innerHTML += " (VOD linked.)<br><br><img src='img/event.png' height=300px><br><br>";
+    workspace[0].innerHTML += "The event started with a few messages from the hosts (?) and 20 minutes of music before the speaker came on stage.<br>";
+    workspace[0].innerHTML += "<img src='img/music.png' height=300px><br>";
 }
 
-function reloadPage(){
-    location.reload();
+function TheSpeaker(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'The Speaker:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "How Did He Act?";
+    workspace[0].innerHTML = "The speaker (Craig Groeschel) has a way of interacting with the people present.<br> ";
+    workspace[0].innerHTML += "<img src='img/craig.png' height='240px'><br><br>";
+    workspace[0].innerHTML += "(1) When he opens up on one of the topics, he involves personal stories and anecdotes.<br>";
+    workspace[0].innerHTML += "(2) He gestures a lot while talking, speaking with his hands.<br>";
+    workspace[0].innerHTML += "(3) He used a few objects, props, during his sermon, such as a book and a mug.<br>";
+    workspace[0].innerHTML += "(4) He would engage in the livestream audience by asking them to comment different things.<br>";
+    workspace[0].innerHTML += "(5) Near the end, he asked people in the audience to grab the shoulder of someone near them.<br>";
+}
+
+function ThePeople(){
+    console.log("changed tabs");
+    let title = document.getElementsByClassName("leftMenuItem");
+    let subtitle = document.getElementsByClassName("menuSubItem");
+    let workspace = document.getElementsByClassName("content");
+    title[0].innerHTML = 'The People:';
+    title[0].innerHTML += '<div class="menuSubItem">\n';
+    subtitle[0].innerText = "How Did They Act?";
+    workspace[0].innerHTML = "There weren't many angles to the seated people, but the live chat was filled with people praying for each other. <br><br>";
+    workspace[0].innerHTML += "<img src='img/livechat1.png' height='240px'><img src='img/livechat2.png' height='240px'><img src='img/livechat3.png' height='240px'><br><br>";
+    workspace[0].innerHTML += "The speaker would occasionally ask a question to the audience, and there would be people answering in the livechat.<br>";
 }
